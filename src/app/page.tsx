228f53cfc3983363062d3c6715311640ff2c6f39
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-11-06 16:06:32 +0600

    generics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,75 +2,30 @@
 import Image from 'next/image'
 
 export default function Home() {
-let fruits: any = ['applee', 'banana', 'mango']
 
-fruits.push(35)
-console.log(fruits)
+  // enums 
+   
+  enum RType {SUCCESS, FAILURE, UNAUTHENTICATED, FORBIDDEN}
 
-
-
-
-
-type stringOrNumber = string | number 
-
-const aNumber : stringOrNumber = '54522'
-
-
-console.log(aNumber)
-
-
-interface circleOptions {
-  width : number;
-  height : number; 
-}
-
-
-function drawCircle (options :circleOptions){
-  let width = options.width; 
-  let height = options.height
-}
-
-let optionsOfdrawCircle = {
-width : 50,
-height : 50
-}
-
-
-console.log(optionsOfdrawCircle)
-
-// declae interface 
-interface optionofsquire {
-  left : number;
-  right :number;
-  up : number;
-  down : number;
-}
-
-// create the function 
-function drawSquire(options:optionofsquire){
-  left : options.left ;
-  right : options.right ;
-  up : options.up ;
-  down : options.down ;
-}
-
-
-// assign the value 
- let valueOfSquire = {
-  left : 40 ,
-  right : 50 ,
-  up : 50 ,
-  down : 50 
+  interface APIResponse<T> {
+  status : number,
+  type : RType;
+  data : T
  }
+ 
+ 
+ const response : APIResponse<string> ={
+  status : 200,
+  type : RType.FORBIDDEN, 
+  data : 'response'
+ 
+}
 
-// call the function 
- console.log(valueOfSquire)
- drawSquire(valueOfSquire)
-
+console.log(response)
 
   return (
    <div>
-    <p>First repo from tys with my repostitory </p>
+    <p>First repo from tys with my repostitorydfd </p>
    </div>
   )
 }
